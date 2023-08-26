c64ca97f9b9216ab424e249ca6ff596222b0f52c
sokolov pavel <sokolovpavel7@gmail.com>
2023-08-26 16:57:08 +0300

    adding product categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
 
 export class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        {
+          key: 'all',
+          name: 'Всё',
+        },
+        {
+          key: 'chairs',
+          name: 'Стулья',
+        },
+        {
+          key: 'tables',
+          name: 'Столы',
+        },
+        {
+          key: 'sofa',
+          name: 'Диваны',
+        },
+        {
+          key: 'light',
+          name: 'Светильники',
+        },
+        {
+          key: 'wall',
+          name: 'Корпусная мебель',
+        },
+      ],
+    };
+  }
   render() {
-    return <div>Categories</div>;
+    return (
+      <div className="categories">
+        {this.state.categories.map((el) => (
+          <div key={el.key} onClick={() => this.props.chooseCategory(el.key)}>
+            {el.name}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
