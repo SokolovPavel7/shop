adb6bf1e6e516cab40ec792ce853cc47e9557449
sokolov pavel <sokolovpavel7@gmail.com>
2023-08-26 11:35:55 +0300

    removing an item from the cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Items from './components/Items';
+import { toHaveAccessibleDescription } from '@testing-library/jest-dom/matchers';
 
 class App extends React.Component {
   constructor(props) {
@@ -60,15 +61,20 @@ class App extends React.Component {
       ],
     };
     this.addToOrder = this.addToOrder.bind(this);
+    this.deleteOrder = this.deleteOrder.bind(this);
   }
   render() {
     return (
       <div className="wrapper">
-        <Header orders={this.state.orders} />
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
         <Items items={this.state.items} onAdd={this.addToOrder} />
         <Footer />
       </div>
     );
+  }
+
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter((el) => el.id !== id) });
   }
 
   addToOrder(item) {
